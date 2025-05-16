0cf2967b4374e33ed6c01adc233a270dacbecd8d
domsdevnest <domsdevnest@gmail.com>
2025-05-16 15:14:41 +0900

    템플릿 메소드 패턴 적용

--- advanced/src/test/java/com/domsdevnest/advanced/trace/template/TemplateMethodTest.java
+++ advanced/src/test/java/com/domsdevnest/advanced/trace/template/TemplateMethodTest.java
@@ -1,4 +1,42 @@
 package com.domsdevnest.advanced.trace.template;
 
+import com.domsdevnest.advanced.trace.template.code.AbstractTemplate;
+import com.domsdevnest.advanced.trace.template.code.SubClassLogic1;
+import com.domsdevnest.advanced.trace.template.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
 public class TemplateMethodTest {
+    @Test
+    void templateMethodV0() {
+        logic1();
+        logic1();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+        log.info("비즈니스 로직 1 실행");
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+        log.info("비즈니스 로직 2 실행");
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+
+    /**
+     * 템플릿 메소드 패턴 적용
+     */
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
+    }
 }
