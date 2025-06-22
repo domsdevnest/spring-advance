7d14abc9d4977375cea656fe57b14b7b3a7013be
domsdevnest <domsdevnest@gmail.com>
2025-06-22 14:58:46 +0900

    로직은 수정하지 않고 프록시 객체를 통해 캐시를 적용

--- proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,8 +1,11 @@
 package hello.proxy.pureproxy.proxy;
 
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
 import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import hello.proxy.pureproxy.proxy.code.RealSubject;
+import hello.proxy.pureproxy.proxy.code.Subject;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.web.WebProperties;
 
 public class ProxyPatternTest {
 
@@ -10,6 +13,16 @@ public class ProxyPatternTest {
     void noProxyTest() {
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
         client.execute();
         client.execute();
         client.execute();
