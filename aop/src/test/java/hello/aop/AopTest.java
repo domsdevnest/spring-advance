c50495a9185a10d5076d4a2544d2f3410b5e1920
domsdevnest <domsdevnest@gmail.com>
2025-07-30 22:29:46 +0900

    기본 적용 방식

--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -2,14 +2,18 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import lombok.extern.slf4j.Slf4j;
+import hello.aop.order.aop.AspectV1;
+import lombok.eAspect 기본 예제	9f6e888	domsdevnest <domsdevnest@gmail.com>	2025. 7. 24. 오후 10:42
+xtern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
+@Import(AspectV1.class) //빈 등록을 위해 추가
 @SpringBootTest
 public class AopTest {
 
