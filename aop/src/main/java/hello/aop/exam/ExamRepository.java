0cf45d4f53b97343a80dbb7dd87db5b5da6d02dd
domsdevnest <domsdevnest@gmail.com>
2025-08-24 21:37:41 +0900

    @Trace를 이용한 로그 출력

--- aop/src/main/java/hello/aop/exam/ExamRepository.java
+++ aop/src/main/java/hello/aop/exam/ExamRepository.java
@@ -1,5 +1,7 @@
 package hello.aop.exam;
 
+import hello.aop.exam.annotation.Trace;
+import jdk.jfr.StackTrace;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,6 +11,7 @@ public class ExamRepository {
     /**
      * 5번에 1번 실패하는 요청
      */
+    @Trace
     public String save(String itemId) {
         seq++;
         if (seq % 5 == 0) {
