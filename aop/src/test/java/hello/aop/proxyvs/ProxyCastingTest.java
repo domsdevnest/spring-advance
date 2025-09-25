cebf91cd5dfe662e247c6f4610b0d74e7996af78
domsdevnest <domsdevnest@gmail.com>
2025-09-25 22:31:21 +0900

    프록시 타입 캐스팅 오류

---
+++ aop/src/test/java/hello/aop/proxyvs/ProxyCastingTest.java
@@ -0,0 +1,35 @@
+package hello.aop.proxyvs;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+
+@Slf4j
+public class ProxyCastingTest {
+    @Test
+    void jdkProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(false);
+
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+        //JDK 동적 프록시를 구현 클래스로 캐스팅 실패
+        Assertions.assertThrows(ClassCastException.class,() -> {
+                    MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+                } );
+
+    }
+
+    @Test
+    void cglibProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true);
+
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+        MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+    }
+}
